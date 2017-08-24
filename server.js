b5702100d25cebef74dece7b9f4b5143ecf8355e
Omkar Vaidya <omi23.vaidya@gmail.com>
2017-08-24 17:47:13 -0400

    playing with the url

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var http = require('http');
+var url = require('url');
 
 var server = http.createServer(function (req, res) {
+	page = url.parse(req.url).pathname;
+	console.log(page);
 	res.writeHead(200, {"Content-Type": "text/html"});
-	res.end('<p><strong><center>Heelloooooo from Node.js :D</center></strong><p>');
+	if(page == '/')
+	{
+		res.write('You are at our Welcome desk. How can I help you??');
+	}
+	else if(page == '/gators')
+	{
+		res.write('Hi Gator! Proud to be Florida Gator! Go Gators!! #ItsGreatUF');
+	}
+	res.end();
 });
 
 server.listen(2307);
